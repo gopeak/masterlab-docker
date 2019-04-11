f4d307937c387c4ca16e9d6573e106db10cde2ad
jugg.walker <jugg.walker@qq.com>
2019-04-11 10:51:46 +0800

    debug

--- v1/apache/build.sh
+++ v1/apache/build.sh
@@ -2,7 +2,7 @@
 
 echo ""
 
-echo -e "\nbuild masterlab image\n"
+echo -e "\nbuild masterlab apache image\n"
 sudo docker build -t masterlab .
 
 
--- v1/fpm/build.sh
+++ v1/fpm/build.sh
@@ -2,8 +2,8 @@
 
 echo ""
 
-echo -e "\nbuild masterlab image\n"
-sudo docker build -t masterlab .
+echo -e "\nbuild masterlab:fpm image\n"
+sudo docker build -t masterlab:fpm .
 
 
 echo ""
