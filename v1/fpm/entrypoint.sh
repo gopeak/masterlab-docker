30b307da9c2ddda05b09985fd1dd04860469fcfd
jugg.walker <jugg.walker@qq.com>
2019-04-11 10:48:20 +0800

    debug

--- v1/fpm/entrypoint.sh
+++ v1/fpm/entrypoint.sh
@@ -3,34 +3,37 @@ set -eu
 
 MASTERLAB_USER=www-data
 MASTERLAB_MOUNT_POINT=/var/www/html
-MASTERLAB_SOURCE_TARGET_ROOT=/var/www/html/masterlab
 
-run_as() {
-    if [ "$(id -u)" = 0 ]; then
-        su -p www-data -s /bin/sh -c "$1"
-    else
-        sh -c "$1"
-    fi
+is_empty_dir(){
+    return `ls -A $1|wc -w`
 }
 
-cp -rf /usr/src/masterlab $MASTERLAB_MOUNT_POINT
-
 # start configure
-cp -f $MASTERLAB_SOURCE_TARGET_ROOT/env.ini-example $MASTERLAB_SOURCE_TARGET_ROOT/env.ini
+if [ "$(id -u)" = 0 ]; then
+    rsync_options="-rDog --chown www-data:root"
+else
+    rsync_options="-rD"
+fi
+
+if [ ! -d $MASTERLAB_MOUNT_POINT ] || is_empty_dir $MASTERLAB_MOUNT_POINT; then
+    rsync $rsync_options /usr/src/masterlab/* $MASTERLAB_MOUNT_POINT
+    cp -f $MASTERLAB_MOUNT_POINT/env.ini-example $MASTERLAB_MOUNT_POINT/env.ini
+fi
+
 echo >&2 "MasterLab has been successfully copied to $PWD"
 
 chown -R $MASTERLAB_USER:$MASTERLAB_USER $MASTERLAB_MOUNT_POINT
 
 mkdir -p /var/spool/cron/crontabs
 cat > /var/spool/cron/crontabs/$MASTERLAB_USER <<EOF
-0,30 22-23 * * * php -f $MASTERLAB_SOURCE_TARGET_ROOT/app/server/timer/project.php
-55 23 * * * php -f $MASTERLAB_SOURCE_TARGET_ROOT/app/server/timer/projectDayReport.php
-56 23 * * * php -f $MASTERLAB_SOURCE_TARGET_ROOT/app/server/timer/sprintDayReport.php
+0,30 22-23 * * * php -f $MASTERLAB_MOUNT_POINT/app/server/timer/project.php
+55 23 * * * php -f $MASTERLAB_MOUNT_POINT/app/server/timer/projectDayReport.php
+56 23 * * * php -f $MASTERLAB_MOUNT_POINT/app/server/timer/sprintDayReport.php
 EOF
 
 # start cron
 cron
 
-echo >&2 "Complete! MasterLab-configure has been successfully installed"
+echo >&2 "Complete! MasterLab-configure Initializing finished"
 
 exec "$@"
